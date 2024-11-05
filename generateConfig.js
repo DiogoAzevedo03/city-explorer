92c2fa3cabab606b91102f9dc435f57aa92b4e3a
DiogoAzevedo03 <diogoazevedo@ipvc.pt>
2024-11-05 21:41:19 +0000

    script para gerar config.js

--- generateConfig.js
+++ generateConfig.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 // Configurações a partir das variáveis de ambiente
 const configContent = `
     const myconfig = {
-        MY_KEY: "${process.env.MY_KEY}",
-        MY_SECRET: "${process.env.MY_SECRET}",
-        MY_KEY_WEATHER: "${process.env.MY_KEY_WEATHER}",
+        MY_KEY: "${process.env.FLICKR_API_KEY}",
+        MY_SECRET: "${process.env.FLICKR_API_SECRET}",
+        MY_KEY_WEATHER: "${process.env.WEATHER_API_KEY}",
         GEONAMES_USERNAME: "${process.env.GEONAMES_USERNAME}"
     };
     export default myconfig;
